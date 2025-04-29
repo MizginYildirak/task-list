85482e84b43a41a75af557a21d4ff49cb74bb5f4
MizginYildirak <mizgin_yldrak@hotmail.com>
2025-04-29 15:35:58 +0300

    add update route

--- routes/web.php
+++ routes/web.php
@@ -19,26 +19,51 @@ Route::get('/tasks', function () {
 
 Route::view("/tasks/create", "create");
 
+
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', ['task' => Task::findOrFail($id)]);
+})->name("tasks.edit");
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task' => Task::findOrFail($id)]);
 })->name("tasks.show");
 
+
 Route::post("/tasks", function (Request $request) {
-  $data = $request->validate([
-    "title" => "required|max:255",
-    "description" => "required",
-    "long_description" => "required",
-  ]);
+    $data = $request->validate([
+        "title" => "required|max:255",
+        "description" => "required",
+        "long_description" => "required",
+    ]);
 
-  $task = new Task;
-  $task->title = $data["title"];
-  $task->description = $data["description"];
-  $task->long_description = $data["long_description"];
-  $task->save();
+    $task = new Task;
+    $task->title = $data["title"];
+    $task->description = $data["description"];
+    $task->long_description = $data["long_description"];
+    $task->save();
 
-  return redirect()->route("tasks.show", ["id" => $task->id])
-  ->with("success", "Task created successfully!");
+    return redirect()->route("tasks.show", ["id" => $task->id])
+        ->with("success", "Task created successfully!");
 })->name("tasks.store");
+
+
+Route::put("/tasks/{id}", function ($id, Request $request) {
+
+    $data = $request->validate([
+        "title" => "required|max:255",
+        "description" => "required",
+        "long_description" => "required",
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data["title"];
+    $task->description = $data["description"];
+    $task->long_description = $data["long_description"];
+    $task->save();
+
+    return redirect()->route("tasks.show", ["id" => $task->id])
+        ->with("success", "Task updated successfully!");
+})->name("tasks.update");
 
 
 // "/hello" is not always a static url, it is rather a URL pattern.
